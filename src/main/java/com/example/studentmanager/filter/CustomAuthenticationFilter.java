0cb78b5fc43e45a588ed7c7de0b9dbfe3a33e60c
nam <nguyentuannam1702@gmail.com>
2021-12-30 11:40:03 +0700

    thay doi CustomAuthenticationFilter

--- src/main/java/com/example/studentmanager/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/studentmanager/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.studentmanager.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @Slf4j
@@ -56,5 +61,11 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withExpiresAt(new Date())
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
+
+        Map<String,String> tokens = new HashMap<>();
+        tokens.put("access_token",access_token);
+        tokens.put("refresh_token",refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(),tokens);
     }
 }
