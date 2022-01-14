e3f8159e0428d6f2f0b6f3b7546c3994cb31ebd9
nam <nguyentuannam1702@gmail.com>
2022-01-14 22:16:14 +0700

    them Outh2Login

---
+++ src/main/java/com/example/studentmanager/oauth/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,32 @@
+package com.example.studentmanager.oauth;
+
+import com.example.studentmanager.entity.User;
+import com.example.studentmanager.security.CustomOAuth2User;
+import com.example.studentmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getName();
+        User user = userService.getUserByEmail(email);
+        System.out.println("Customer's email: " + email);
+        super.onAuthenticationSuccess(request, response, authentication);
+
+    }
+}
